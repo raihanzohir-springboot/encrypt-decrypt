88fd0588fd33830e65209710856c1e5512569c28
Md. Zohir Raihan <zohir@erainfotechbd.com>
2023-07-09 09:22:31 +0600

    Initial Project Structure

---
+++ src/test/java/com/example/encdec/EncryptDecryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encdec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptDecryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
